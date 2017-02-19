a07d906243cd673a7eff743ea59f833037e018ce
Alex <salexln@gmail.com>
2017-02-19 14:04:19 +0200

    adding assignments

---
+++ assignments/machine-learning-ex1/ex1/alex.m
@@ -0,0 +1,7 @@
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+
+plotData(X, y);
---
+++ assignments/machine-learning-ex1/ex1/test.m
@@ -0,0 +1,8 @@
+function A = test()
+
+A = zeros(10, 1);
+for i = 1:10
+  for j = 1:10
+    v(i) = v(i) + A(i, j) * x(j);
+  end
+end
